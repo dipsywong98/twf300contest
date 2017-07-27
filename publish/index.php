e8656f474adb64b85bbf5bbdcfa72b3bd6389050
dipsywong98 <facebookarmyoffcal@gmail.com>
2017-07-27 22:38:55 +0800

    publisher complete

---
+++ publish/index.php
@@ -0,0 +1,60 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+    <title>公開作品 - 300容量挑戰賽</title>
+    <script src="../js/jquery-3.2.1.min.js"></script>
+    <script src="../js/fbservice.js"></script>
+    
+    
+</head>
+
+<body>
+    <?php require 'publish.php'; ?>
+
+    <script>
+    function download(url){
+        var link = document.createElement('a');
+        link.href = url;
+        link.setAttribute('download','<?php echo $hash?>.'+url.split(".")[3]);
+        link.click();
+    }
+    </script>
+    
+    <h2>公開作品</h2>
+        
+    <img width="300px" src="../uploads/<?php echo $hash."/".$hash.".".$photo_type;?>"/>
+    
+    <p>作品名稱：<?php echo $twf_name;?></p>
+    <p>作家編號：<?php echo $hash;?></p>
+    <p>需在<?php 
+        $dt = new DateTime;
+        $dt->setTimeStamp($db->select("publishing","hash",$hash)["expire"]);
+        $dt->setTimeZone(new DateTimeZone("Asia/Hong_Kong"));
+        echo " 香港時間".$dt->format('Y年m月d日 H:i:s');
+        ?>之前公開，否則會有下一名評審代為公開</p>
+    <button onclick="download('<?php echo"../uploads/".$hash."/".$hash.".twf";?>')">下載twf檔</button>
+    <button onclick="download('<?php echo"../uploads/".$hash."/".$hash.".".$photo_type;?>')">下載圖片檔</button>
+    
+    <hr>
+    <h2>公開後請填寫此表</h2>
+    
+    <form method="post" enctype="multipart/form-data" action='<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>'>
+        
+        <input type="hidden" name="hash">
+        
+        <p>任務代碼</p>
+        <input id="input_pw" type="number" name="mission_id">
+        <br><br> 
+        
+        <p>再輸入一次任務代碼</p>
+        <input id="input_pw" type="number" name="mission_id_prove">
+        <br><br> 
+        
+        <p>純文字檔</p>
+        <input id="txt" type="file" name="txt_file" accept=".txt">
+        <br><br>
+        
+        <input type="submit" name="submit" value="Submit">
+    </form>
+</body>
+</html>
---
+++ publish/publish.php
@@ -0,0 +1,119 @@
+<?php
+
+require "../includes/helper.php";
+
+//echo time();
+
+if(!isLogin())
+    redirect("../login");
+else{
+    if(!isAdmin()){
+        alert("你未獲得授權");
+        redirect("../");
+        die();
+    }
+}
+
+//finding a submit to publish
+$submit ="";
+
+$record = $db->select("publishing","pic",getLoginUsername());
+if($db->numberOf("publishing","pic",getLoginUsername())){
+    //if user is pic for a publish event already, get his pic record
+    $submit = $db->select("submits","hash",$record["hash"]);
+//    echo "this guy have a publish record <br>";
+}
+else{
+    //if user is not any pic
+    //find all submits haven't publish
+    $records = $db->selectAll("submits","mission_id","-1");
+    foreach($records as $record){
+        
+        //if this submit do not have pic, be the pic
+        if($db->numberOf("publishing","hash",$record["hash"])==0){
+            $submit = $record;
+//            echo "new";
+            $db->insert("publishing",[
+                "hash"=>$record["hash"],
+                "pic"=>getLoginUsername(),
+                "expire"=>time()+3600*24*1.5
+            ]);
+            break;
+        }
+        else{
+            
+            //if this submit have a pic and it is a freerider, take over his job
+            $publish = $db->select("publishing","hash",$record["hash"]);
+            if(isExpired($publish)){
+//                echo "take over";
+                $submit = $record;
+                $db->update("publishing",$record["hash"],[
+                    "pic"=>getLoginUsername(),
+                    "expire"=>time()+3600*24*1.5
+                ]);
+                break;
+            }
+        }
+        
+        
+    }
+}
+
+if($submit==""){
+    alert("no submition need publish now");
+    redirect("../");
+}
+
+$twf_name = $submit["twf_name"];
+$hash = $submit["hash"];
+$photo_type = $submit["photo_type"];
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    
+    if($db->numberOf("publishing","pic",getLoginUsername())==0){
+        alert("代碼為".$_POST["hash"]."的作品已被公開");
+        redirect("");
+        die();
+    }
+    
+    if($_POST["mission_id"]==""){
+        alert("請輸入任務代碼");
+        redirect("");
+        die();
+    }
+    if($_POST["mission_id"]!=$_POST["mission_id_prove"]){
+        alert("兩個任務代碼不相等");
+        redirect("");
+        die();
+    }
+    if($_FILES["txt_file"]["error"]==4){
+        alert("請上載純文字檔");
+        redirect("");
+        die();
+    }elseif($_FILES["txt_file"]["error"]!=0){
+        alert("上載純文字檔時發生錯誤，代碼".$_FILES["txt_file"]["error"]);
+        redirect("");
+        die();
+    }else if(!in_array(pathinfo($_FILES['txt_file']['name'], PATHINFO_EXTENSION),["txt"])){
+        alert("請確保你上載的是文字檔");
+        redirect("");
+        die();
+    }
+    
+    move_uploaded_file($_FILES["txt_file"]["tmp_name"],"../uploads/".$hash."/".$hash.".txt");
+    
+    $mission_id = $_POST["mission_id"];
+    $db->update("submits",$hash,[
+        "mission_id"=>$mission_id
+    ]);
+    $db->delete("publishing",$hash);
+    
+    alert("success!");
+//    redirect("/");
+}
+
+function isExpired($record){
+    return time()>$record["expire"];
+}
+
+?>
