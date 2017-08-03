23c4c7953909fe27e7850d71c62fe98dd472fe13
Jamie Phan <jamie_phan@hotmail.com>
2017-08-04 00:44:05 +0800

    fix index.php

--- index.php
+++ index.php
@@ -1,3 +1,7 @@
+<?php
+require "includes/helper.php";
+?>
+
 <html>
 <head>
 <title>首頁 - 300容量挑戰賽</title> 
@@ -19,7 +23,7 @@
         <main>
     這是首頁
     <?php 
-    require "includes/helper.php";
+    
     echo isLogin();
     ?>
     <p><a href="login/"> 登入 </a>|<a href="submit/"> 遞交 </a>|<a href="publish/"> 公開 </a>|<a href="vote/"> 評分 </a>|<a href="profile/"> 我的作品 | 我的評賞 </a>|<a href="control_panel.php"> 控制台 </a>|<a href="logout.php"> 登出 </a></p>
