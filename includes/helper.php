17b181b765c53641b1ae7cecf8dcfb7a90b618d8
dipsywong98 <facebookarmyoffcal@gmail.com>
2017-08-11 22:53:23 +0800

    update admin list

--- includes/helper.php
+++ includes/helper.php
@@ -94,7 +94,7 @@ session_start();
     }
 
     function isAdmin(){
-        $admin=["twf300_2017","facebookoffical","facebook_002"];
+        $admin=["twf300_2017","a230459446","ericgau22","100002331046844%40facebook.com","100003919138920%40facebook.com","100000913023977%40facebook.com"];
         return in_array(getLoginUsername(),$admin);
     }
 
